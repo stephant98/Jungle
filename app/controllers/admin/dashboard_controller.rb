ee4124a80e2903da943bb7aa8ceaa02e5db10ca3
Stephan Truchsess <stephantruchsess@gmail.com>
2021-03-03 18:13:39 -0500

    Add totals to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
   def show
+    @products = Product.count 
+    @categories = Category.count
   end
 end
