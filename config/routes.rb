4811addf5dbee57c82043c2977dd9a0e8962ec94
Stephan Truchsess <stephantruchsess@gmail.com>
2021-03-04 18:00:26 -0500

    Add user authentication condition

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
   resources :about, only: [:index]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
